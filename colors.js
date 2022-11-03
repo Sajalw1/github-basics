42ea569b0eb0f7663283035d9ae2208a4f01b769
Sajalw1 <sajal.wagley@gmail.com>
2022-11-03 08:15:02 +0545

    Initial Commit

---
+++ colors.js
@@ -0,0 +1,7 @@
+import {modifierNames, foregroundColorNames} from 'chalk';
+
+console.log(modifierNames.includes('bold'));
+//=> true
+
+console.log(foregroundColorNames.includes('pink'));
+//=> false
